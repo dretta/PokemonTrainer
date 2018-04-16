ab220f4a921e78d075fb5e75513b695c9d25638a
dretta <dretta@ymail.com>
2018-04-16 03:14:07 -0600

    Adjusted source organization

--- pokemontrainer/core.py
+++ pokemontrainer/core.py
@@ -1,21 +1,5 @@
 # -*- coding: utf-8 -*-
 from pokemontrainer import helpers
 
-
-import streamlink
-import subprocess
-import requests
-
 if __name__ == "__main__":
-    streams = streamlink.streams("http://twitch.tv/twitchplayspokemon")
-    stream = streams["worst"]
-    fd = stream.open()
-    url = fd.writer.stream.url
-    fd.close()
-    res = requests.get(url)
-    tsFiles = list(filter(lambda line: line.startswith('http'), res.text.splitlines()))
-    print(tsFiles)
-    for i, ts in enumerate(tsFiles):
-        vid = 'vid{}.ts'.format(i)
-        subprocess.run(['curl', ts, '-o', vid])
-        subprocess.run(['ffmpeg', '-i', vid, '-vf', 'fps=1', 'out{}_%d.png'.format(i)])
+    helpers.getLiveStreamImages()
--- pokemontrainer/helpers.py
+++ pokemontrainer/helpers.py
@@ -1,3 +1,26 @@
-def get_answer():
-    """Get an answer."""
-    return True
+import streamlink
+import subprocess
+import requests
+
+
+def getLiveStreamImages():
+    streams = streamlink.streams("http://twitch.tv/twitchplayspokemon")
+    stream = streams["worst"]
+    fd = stream.open()
+    url = fd.writer.stream.url
+    fd.close()
+    res = requests.get(url)
+    tsFiles = list(filter(lambda line: line.startswith('http'), res.text.splitlines()))
+    print(tsFiles)
+    for i, ts in enumerate(tsFiles):
+        vid = 'vid{}.ts'.format(i)
+        subprocess.run(['curl', ts, '-o', vid])
+        subprocess.run(['ffmpeg', '-i', vid, '-vf', 'fps=1', 'out{}_%d.png'.format(i)])
+
+
+def getVideo(url, length):
+    subprocess.run(['streamlink', url, 'best', '-O', '|', 'ffmpeg', '-i', 'pipe:0', '-t', length, 'battle.ts'])
+
+def getNFrames(n):
+    subprocess.run(['ffmpeg', '-i', 'battle.ts', '-vf', 'select=not(mod(n\,{}))'.format(n), '-vsync', 'vfr', '-q:v',
+                    '2', 'img_%03d.jpg'])
