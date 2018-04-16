9ae6437f51f7ac4988031637983152dcaa911ca8
dretta <dretta@ymail.com>
2018-04-16 00:22:44 -0600

    Implemented steam to image sequence example

--- pokemontrainer/core.py
+++ pokemontrainer/core.py
@@ -2,12 +2,20 @@
 from pokemontrainer import helpers
 
 
-def get_hmm():
-    """Get a thought."""
-    return 'hmmm...'
+import streamlink
+import subprocess
+import requests
 
-
-def hmm():
-    """Contemplation..."""
-    if helpers.get_answer():
-        print(get_hmm())
+if __name__ == "__main__":
+    streams = streamlink.streams("http://twitch.tv/twitchplayspokemon")
+    stream = streams["worst"]
+    fd = stream.open()
+    url = fd.writer.stream.url
+    fd.close()
+    res = requests.get(url)
+    tsFiles = list(filter(lambda line: line.startswith('http'), res.text.splitlines()))
+    print(tsFiles)
+    for i, ts in enumerate(tsFiles):
+        vid = 'vid{}.ts'.format(i)
+        subprocess.run(['curl', ts, '-o', vid])
+        subprocess.run(['ffmpeg', '-i', vid, '-vf', 'fps=1', 'out{}_%d.png'.format(i)])
